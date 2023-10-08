04ba6596ab7fa2b6931708f6ed74ef433c9f6541
Gia Bao <21521866@gm.uit.edu.vn>
2023-10-08 15:45:49 +0700

    header ui

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,11 +1,15 @@
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
 
+const cx = classNames.bind(styles);
+
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
                 <div className="content">{children}</div>
             </div>
